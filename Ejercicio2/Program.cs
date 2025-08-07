4af7ab9b192e298bb4bedc0f6c9302cccbbfc34b
goethe123 <goethe123123@gmail.com>
2025-08-07 10:37:28 -0600

    feat: person constructor updates

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collecions.Generic;
+using System.Collections.Generic;
 namespace Ejercicio2
 {
     // Tema: Clases, instanciación, getters y setters, constructores
@@ -10,7 +10,13 @@ namespace Ejercicio2
     {
         public static void Main(string[] args)
         {
-            Persona persona1 = new Persona();
+            Persona persona1 = new Persona("Carlitos", 19, "Canada");
+            Console.WriteLine($"Nombre: {persona1.GetNombre()}\nEdad: {persona1.GetEdad()}\nCiudad {persona1.GetCiudad()}");
+
+            persona1.SetEdad(20);
+            persona1.SetCiudad("Guadalajara");
+            Console.WriteLine("Despues de modificar: ");
+            Console.WriteLine($"Nombre: {persona1.GetNombre()}\n Edad: {persona1.GetEdad()}\n Ciudad {persona1.GetCiudad()}");
         }
     }
     public class Persona
@@ -19,7 +25,7 @@ namespace Ejercicio2
         private int edad;
         private string ciudad;
 
-        public
+        
 
         //contstructor
         public Persona(string Nombre, int Edad, string Ciudad)
@@ -31,36 +37,36 @@ namespace Ejercicio2
 
         //Getters 
 
-        public GetNombre()
+        public string GetNombre()
         {
-            return Nombre;
+            return nombre;
         }
 
-        public GetEdad()
+        public int GetEdad()
         {
-            return Edad;
+            return edad;
         }
 
-        public GetCiudad()
+        public string GetCiudad()
         {
-            return Ciudad;
+            return ciudad;
         }
 
         //SETTERS
 
         public void SetNombre(string NuevoNombre)
         {
-            Nombre = NuevoNombre;
+            nombre = NuevoNombre;
         }
 
         public void SetEdad(int NuevaEdad)
         {
-            Edad = NuevaEdad;
+            edad = NuevaEdad;
         }
 
         public void SetCiudad(string NuevaCiudad)
         {
-            Ciudad = NuevaCiudad;
+            ciudad = NuevaCiudad;
         }
     }
 
