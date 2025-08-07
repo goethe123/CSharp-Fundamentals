c8b46c165c514131debeace101d1cb49f2acbdea
goethe123 <goethe123123@gmail.com>
2025-08-07 09:37:44 -0600

    feat: add method to print multiplication table from 1 to 10.

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Ejercicio1
+{
+
+    // Ejercicio 1: Tabla de multiplicar (Bucles, Métodos)
+    // Tema: Métodos, bucles, condicionales
+    // Descripción:
+    // Crea un método que reciba un número y muestre su tabla de multiplicar del 1 al 10.
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("dame el numero que quieres para multiplicar");
+            int numUser = Convert.ToInt32(Console.ReadLine());
+            Multiplicación(numUser);
+        }
+
+        public static void Multiplicación(int num)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} x {i} = {num * i}");
+            }
+        }
+    }
+}
