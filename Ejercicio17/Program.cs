c912daa76baf7e80f13d9194c039a1d4407b878d
goethe123 <goethe123123@gmail.com>
2025-08-13 10:30:43 -0600

    feat: contar vocales

---
+++ Ejercicio17/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// Contar vocales en una cadena
+// Pide al usuario una palabra y devuelve cuántas vocales tiene.
+// Tema: string, bucles, condicionales
+
+using System;
+using System.Collections.Generic;
+namespace ejercicio17
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("dame una palabra para contar sus vocales");
+            string palabra = Console.ReadLine();
+            int vocales = 0;
+            string minuscula = palabra.ToLower();
+            foreach (char letra in minuscula)
+            {
+                if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'u' || letra == 'o')
+                {
+                    vocales++;
+                }
+            }
+            Console.WriteLine("tu palabra " + palabra + " tiene " + vocales + " vocales");
+        }
+     }
+}
