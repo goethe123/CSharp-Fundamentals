dd41854380d6862df4d681d3b4c8e938a8a35e9e
goethe123 <goethe123123@gmail.com>
2025-08-13 10:51:16 -0600

    feat: invertir palabra

---
+++ ejercicio18/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.Arm;
+namespace ejercicio18
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            System.Console.WriteLine("dame una palabra");
+            string palabra = Console.ReadLine();
+
+            int Longitud = palabra.Length;
+            char[] letras = new char[Longitud];
+            int contador = 0;
+            for (int i = Longitud - 1; i >= 0; i--)
+            {
+                letras[contador] = palabra[i];
+                contador++;
+            }
+
+            Console.Write(letras);
+            
+
+        }
+    }
+}
