32bba7d87469d8ed0c8f7c54c7f677cd1c2b709f
goethe123 <goethe123123@gmail.com>
2025-08-13 22:22:14 -0600

    feat enum

---
+++ ejercicio20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ejercicio29
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Empleado gus = new Empleado(1200, 250);
+            gus.CambiarSalarioBase(gus, 100);
+            System.Console.WriteLine(gus);
+
+        }
+    }
+
+    public class Empleado
+    {
+        public double salarioBase, comision;
+
+        public Empleado(double salarioBase, double comision)
+        {
+            this.salarioBase = salarioBase;
+            this.comision = comision;
+        }
+        public override string ToString()
+        {
+            return string.Format("el salario base de este empleado es {0} y su comision es de {1}", salarioBase, comision);
+        }
+
+        public void CambiarSalarioBase(Empleado emp, double incremento)
+        {
+            emp.salarioBase += incremento;
+            emp.comision += incremento;
+        }
+
+    }
+    
+}
