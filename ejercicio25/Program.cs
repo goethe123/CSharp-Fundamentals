4555926b5fd34ac801f0cdce27f15715492456ca
goethe123 <goethe123123@gmail.com>
2025-08-14 14:12:32 -0600

    feat: collections List 1st example

---
+++ ejercicio25/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ejercicio26
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("cuantos elementos quieres añadir?");
+            int numeros = Convert.ToInt32(Console.ReadLine());
+
+            List<int> NumsList = new List<int>();
+
+            for (int i = 0; i < numeros; i++)
+            {
+                System.Console.WriteLine("añade el elemento de la posicion " + i);
+                NumsList.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < numeros; i++)
+            {
+                Console.Write(NumsList[i]);
+            }
+            
+        }
+    }
+}
