29644f904b7aedd1b4a52edd89674560fc0020ca
goethe123 <goethe123123@gmail.com>
2025-08-14 01:14:59 -0600

    feat enum excersise

---
+++ ejercicio21/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ejercicio21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado antonio = new Empleado(Bonus.normal, 20000);
+            antonio.MostrarInfo();
+
+            Empleado michel = new Empleado(Bonus.extra, 1500);
+            michel.MostrarInfo();
+        }
+    }
+
+    public class Empleado
+    {
+        private double bonus;
+        private double salario;
+
+        public Empleado(Bonus bonusEmpleado, double salario)
+        {
+            bonus = (double)bonusEmpleado;
+            this.salario = salario;
+        }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Salario base: {salario}");
+            Console.WriteLine($"Bonus: {bonus}");
+            Console.WriteLine($"Total: {salario + bonus}");
+        }
+
+        
+    }
+
+   public enum Bonus { bajo = 500, normal = 1000, bueno = 1500, extra = 3000 };
+}
