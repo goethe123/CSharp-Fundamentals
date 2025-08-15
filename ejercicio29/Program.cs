6ff52ffefc819ca03bb94906942d01cc46757f8d
goethe123 <goethe123123@gmail.com>
2025-08-15 17:28:03 -0600

    dictionary lesson 2

---
+++ ejercicio29/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ejercicio29
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, int> Edades = new Dictionary<string, int>();
+
+            //rellenar nuestra dictionary collection
+
+            //llenar dictionary usando el .Add
+            Edades.Add("Juan", 18);
+            Edades.Add("Pepe", 28);
+
+            //llenar dictionary como s fuera un array
+            Edades["Lopez"] = 20;
+            Edades["jacobo"] = 37;
+
+            foreach (KeyValuePair<string, int> persona in Edades)
+            {
+                Console.WriteLine($"nombre : {persona.Key} \n edad : {persona.Value} \n"); 
+            }
+        }
+    }
+}
