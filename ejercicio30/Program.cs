c410298b69a0a98297c25600f08e2298a13a4feb
goethe123 <goethe123123@gmail.com>
2025-08-15 18:34:42 -0600

    feat: delegates example

---
+++ ejercicio30/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ejercicio39
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ObjetoDelegado Delegado = new ObjetoDelegado(MensajeSaludo.SaludoBienvenida);
+            Delegado("soy el goet");
+
+            Delegado = new ObjetoDelegado(MensajeDespedida.SaludoDespedida);
+            Delegado(" fui el goet");
+
+        }
+
+        delegate void ObjetoDelegado(string msj);
+    }
+
+    public class MensajeSaludo
+    {
+        public static void SaludoBienvenida(string msj)
+        {
+            System.Console.WriteLine("hola hola buenos diías" + msj);
+        }
+    }
+
+    public class MensajeDespedida
+    {
+        public static void SaludoDespedida(string msj)
+        {
+            System.Console.WriteLine("bueno ya me despido muchas gracias"+msj);
+        }
+    }
+}
