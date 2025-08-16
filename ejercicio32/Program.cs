9b7c7a56311e745684c7d354c966cf55114aa366
goethe123 <goethe123123@gmail.com>
2025-08-15 22:22:41 -0600

    feat: ejercico predicates

---
+++ ejercicio32/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Reflection.Metadata;
+
+namespace ejercicio32
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Persona> personas = new List<Persona>();
+            Persona p1 = new Persona();
+            p1.Edad = 21;
+            p1.Nombre = "Juan";
+
+            Persona p2 = new Persona();
+            p2.Edad = 25;
+            p2.Nombre = "Pepe";
+
+            Persona p3 = new Persona();
+            p3.Edad = 21;
+            p3.Nombre = "Gomez";
+
+            personas.AddRange(new Persona[] { p1, p2, p3 });
+
+            Predicate<Persona> Predicado = new Predicate<Persona>(ExisteJuan);
+            bool ExisteJuanes = personas.Exists(ExisteJuan);
+
+            if (ExisteJuanes)
+            {
+                System.Console.WriteLine("si hay juanes en tu lista de personas");
+            }
+            else
+            {
+                System.Console.WriteLine("no hay juanes en tu lista");
+            }
+
+        }
+
+        public static bool ExisteJuan(Persona persona)
+        {
+            if (persona.Nombre == "Juan")
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class Persona
+    {
+        private int edad;
+        private string nombre;
+
+        public string Nombre { get => nombre; set => nombre = value; } 
+        
+        public int Edad { get => edad; set => edad = value; }
+
+
+
+    }
+}
