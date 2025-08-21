ddeab3036caa68c968eff36ab82590a5bcd8ffd8
goethe ramirez <goethe123123@gmail.com>
2025-08-20 18:12:45 -0600

    feat: radioButton lesson, we made a traffic light

---
+++ MiTerceraInterfaz/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MiTerceraInterfaz;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void RadioButton_Click(object sender, RoutedEventArgs e)
+    {
+        eRojo.Visibility=Visibility.Visible;
+        eAmarillo.Visibility = Visibility.Hidden;
+        eVerde.Visibility = Visibility.Hidden;
+
+    }
+
+
+    private void RadioButton_Click_1(object sender, RoutedEventArgs e)
+    {
+        eAmarillo.Visibility = Visibility.Visible;
+        eVerde.Visibility = Visibility.Hidden;
+        eRojo.Visibility = Visibility.Hidden;
+
+
+    }
+
+    private void RadioButton_Click_2(object sender, RoutedEventArgs e)
+    {
+        eVerde.Visibility = Visibility.Visible;
+        eAmarillo.Visibility = Visibility.Hidden;
+        eRojo.Visibility = Visibility.Hidden;
+
+
+    }
+}
