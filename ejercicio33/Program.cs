168fe285573b0c11a460975c30b812c0cc76520c
goethe ramirez <goethe123123@gmail.com>
2025-08-26 10:17:12 -0600

    feat: LINQ Example

---
+++ ejercicio33/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ejercicio33
+{
+        public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Numeros Pares");
+            int[] Numeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            /*
+            List <int> NumerosPares = new List <int> ();
+
+            foreach (int i in Numeros)
+            {
+                if (i %2 ==0)
+                {
+                    NumerosPares.Add (i);
+                }
+            }
+            */
+            IEnumerable <int> NumerosPares = from numero in Numeros where numero %2 == 0 select numero;
+
+            foreach (int i in NumerosPares)
+            {
+                Console.Write(i+ ",");
+            }
+            
+        }
+    }
+
+    
+}
